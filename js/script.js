dc3412877f26f0232d239cec9a6305bec458d68c
Steve Kaci <kaci.steve@k-net.fr>
2021-12-24 18:41:58 +0100

    the end

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function slideNav() {
+  var slideDisplay = document.getElementById("small-slide");
+
+  if (slideDisplay.style.display == "block") {
+    slideDisplay.style.display = "none";
+  } else {
+    slideDisplay.style.display = "block";
+  }
+}
