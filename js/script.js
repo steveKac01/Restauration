6bb80e84b5ef6b36c9fa34b1b211553f265bc4d5
Steve Kaci <kaci.steve@k-net.fr>
2022-01-14 19:58:58 +0100

    fix js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-function slideNav() {
-  var slideDisplay = document.getElementById("small-slide");
+let slideDisplay = document.querySelector("#small-slide");
+let linkSmallSlideMenu = document.querySelectorAll(".slide-cat a");
+let btnBurger = document.querySelector(".burger");
 
-  if (slideDisplay.style.display == "block") {
-    slideDisplay.style.display = "none";
-  } else {
-    slideDisplay.style.display = "block";
-  }
+//Event
+btnBurger.addEventListener("click", showHide);
+for (var i=0; i < linkSmallSlideMenu.length - 1; i++) {
+  linkSmallSlideMenu[i].addEventListener("click", showHide);
 }
+
+function showHide(){
+  slideDisplay.classList.toggle("hide");
+  console.log("trytohide");
+}
+
+   
