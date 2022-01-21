cb58253a687b45ed94f822382884dcfb7987a479
Steve Kaci <kaci.steve@k-net.fr>
2022-01-21 21:10:02 +0100

    fix burger

--- js/script.js
+++ js/script.js
@@ -4,13 +4,22 @@ let btnBurger = document.querySelector(".burger");
 
 //Event
 btnBurger.addEventListener("click", showHide);
-for (var i=0; i < linkSmallSlideMenu.length - 1; i++) {
+for (var i = 0; i < linkSmallSlideMenu.length - 1; i++) {
   linkSmallSlideMenu[i].addEventListener("click", showHide);
 }
 
-function showHide(){
+function showHide() {
   slideDisplay.classList.toggle("hide");
   console.log("trytohide");
 }
 
-   
+const mQ = window.matchMedia("(min-width: 768px)");
+
+mQ.addListener(function (x) {
+  if (x.matches) {
+    if (!slideDisplay.classList.contains("hide")) {
+      slideDisplay.classList.toggle("hide");
+    }
+  } else {
+  }
+});
